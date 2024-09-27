95267480f8b8a162474668a7877898d94434561f
DaanJesus <danilo25oliveira@gmail.com>
2024-09-27 13:44:47 -0300

    teste vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { LoadingService } from './services/loading.service';
-import { TimelineMax, Power2, Power4 } from 'gsap';
-import * as $ from 'jquery';
-import { filter, timestamp } from 'rxjs';
-import { NavigationEnd, Route, Router } from '@angular/router';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
